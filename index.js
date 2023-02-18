84300b58710422d7574f50b8d10c2f8ee366246c
JubayerDev <ajashes858@gmail.com>
2023-02-18 15:47:35 +0600

    post like unlike

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json())
+
+
+
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const postCollection = client.db("connectVerse").collection("posts");
+
+        app.post('/posts', async (req, res) => {
+            const post = req.body;
+
+            const result = await postCollection.insertOne(post);
+            res.send(result);
+        })
+
+        app.get("/posts", async (req, res) => {
+            const posts = await postCollection.find({}).toArray();
+            res.send(posts);
+        })
+
+        app.get("/posts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const post = await postCollection.findOne(query);
+            res.send(post);
+        })
+
+        app.post("/posts/:postId/like", async (req, res) => {
+            const userId = req.body.userId;
+            const postId = req.params.postId;
+
+
+            postCollection.findOne({ _id: new ObjectId(postId) }, (err, post) => {
+                if (err) {
+                    res.status(500).send(err);
+                    return;
+                }
+
+                // chk if post already liked
+                const alreadyLiked = post.likes.find((like) => like.userId === userId);
+
+                if (alreadyLiked) {
+                    postCollection.updateOne({ _id: new ObjectId(postId) }, { $pull: { likes: { userId: userId } } }, (err) => {
+                        if (err) {
+                            res.status(500).send(err)
+                        } else {
+                            res.status(200).send('Post unliked');
+                        }
+                    })
+                } else {
+                    // like post
+                    const like = { userId: userId, postId: postId }
+
+                    postCollection.updateOne({ _id: new ObjectId(postId) }, { $push: { likes: like } }, (err) => {
+                        if (err) {
+                            res.status(500).send(err);
+                        }
+                        else {
+                            res.status(200).send('Post liked');
+                        }
+                    })
+                }
+            })
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('App is running')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`app is listening to port on ${port}`);
+})
